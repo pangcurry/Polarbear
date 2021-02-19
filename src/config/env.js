6d6baf71f12cde9de6b852442f482187eaa4cd57
pangcurry <magicshuffle2424@hotmail.com>
2021-02-19 17:39:02 +0900

    [Refactor] separation extension from main

--- src/config/env.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-module.exports = {
-    port: process.env.SERVER_PORT,
-    mp3_path: process.env.MP3_PATH,
-    script_path: process.env.SCRIPT_PATH,
-    timeout: process.env.TIMEOUT
-}
