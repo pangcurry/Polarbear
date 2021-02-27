9900c23042e447f2ba831066b9daec1e481a0b32
pangcurry <magicshuffle2424@hotmail.com>
2021-02-27 22:35:35 +0900

    [Add] add video_table from env config

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,9 @@
-require('dotenv').config()
+ require('dotenv').config();
 
 module.exports = {
     port: process.env.SERVER_PORT,
     mp3_path: process.env.MP3_PATH,
     script_path: process.env.SCRIPT_PATH,
-    timeout: process.env.TIMEOUT
-}
+    timeout: process.env.TIMEOUT,
+    video_table: process.env.VIDEO_TABLE
+};
