28724a1a4a921f0091106fc97f6baef44aa8a76d
두평신 <magicshuffle2424@hotmail.com>
2023-01-09 03:33:47 +0900

    [hotfix] encoding 수정, ui 수정

--- src/util/getDownloadFilename.js
+++ src/util/getDownloadFilename.js
@@ -3,16 +3,16 @@ import iconvLite from "iconv-lite";
 export const getDownloadFilename = (req, fileName) => {
   const header = req.headers["user-agent"];
   console.log("user-header: ", header);
-
-  if (header.includes("MSIE") || header.includes("Trident")) {
-    return encodeURIComponent(fileName).replace(/\\+/gi, "%20");
-  } else if (header.includes("Chrome")) {
-    return iconvLite.decode(iconvLite.encode(fileName, "UTF-8"), "ISO-8859-1");
-  } else if (header.includes("Opera")) {
-    return iconvLite.decode(iconvLite.encode(fileName, "UTF-8"), "ISO-8859-1");
-  } else if (header.includes("Firefox")) {
-    return iconvLite.decode(iconvLite.encode(fileName, "UTF-8"), "ISO-8859-1");
-  } else {
-    return encodeURIComponent(fileName).replace(/\\+/gi, "%20");
-  }
+  return encodeURIComponent(fileName).replace(/\\+/gi, "%20");
+  // if (header.includes("MSIE") || header.includes("Trident")) {
+  //   return encodeURIComponent(fileName).replace(/\\+/gi, "%20");
+  // } else if (header.includes("Chrome")) {
+  //   return iconvLite.decode(iconvLite.encode(fileName, "UTF-8"), "ISO-8859-1");
+  // } else if (header.includes("Opera")) {
+  //   return iconvLite.decode(iconvLite.encode(fileName, "UTF-8"), "ISO-8859-1");
+  // } else if (header.includes("Firefox")) {
+  //   return iconvLite.decode(iconvLite.encode(fileName, "UTF-8"), "ISO-8859-1");
+  // } else {
+  //   return encodeURIComponent(fileName).replace(/\\+/gi, "%20");
+  // }
 };
